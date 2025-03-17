25edc7ca52c13765e76cb70e3ea8dfa0bc3a277f
shuvankr7 <92281019+shuvankr7@users.noreply.github.com>
2025-03-17 08:59:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-!pip install spacy
+pip install spacy
 python -m spacy download en_core_web_sm
-!pip install json
-!pip install re
+pip install json
+pip install re
 import re
 import json
 import spacy
